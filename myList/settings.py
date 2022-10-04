7562061b4c2ed28eab995ad19a89c2cf1b8dcdfe
Jenny Gardner <j.rongjie@gmail.com>
2022-10-04 08:33:37 -0400

    update security

--- myList/settings.py
+++ myList/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@wv6xz&@v^kqm3-#-7h&)vlon%9m(wi1%f^qxqw41&d#+l@$s)'
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
